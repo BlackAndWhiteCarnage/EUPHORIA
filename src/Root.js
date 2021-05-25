49896a159f3e29ecb7a4bfdc13e7770d2ca930d2
Krzysztof Repsch <krzys.repsch@gmail.com>
2021-05-25 20:00:31 +0200

    Added assets, theme and  global styles

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+
+const Root = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default Root;
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    crimsonRed: '#B80202',
+    darkGrey: '#2D2D2D',
+    darkWhite: '#F9F9F9',
+  },
+  fontSize: {
+    xl: '28px',
+    l: '24px',
+    m: '14px',
+    s: '12px',
+  },
+};
